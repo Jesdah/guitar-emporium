5ecaa8a7e5a8eebe1f2f293dc8eb21c17db6ac7a
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-10-01 18:29:21 +0000

    add urls to guitars

--- guitars/urls.py
+++ guitars/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.all_guitars, name='guitars'),
     path('<int:guitar_id>/', views.guitar_detail, name='guitar_detail'),
-    # path('add/', views.add_product, name='add_product'),
-    # path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-    # path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('add/', views.add_guitar, name='add_guitar'),
+    path('edit/<int:guitar_id>/', views.edit_guitar, name='edit_guitar'),
+    path('delete/<int:guitar_id>/', views.delete_guitar, name='delete_guitar'),
 ]
