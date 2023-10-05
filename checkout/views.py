239baba519beb01a52e0b15f85dc54a4cc476856
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-10-05 19:08:59 +0000

    create checkout success page

--- checkout/views.py
+++ checkout/views.py
@@ -1,35 +1,43 @@
+import json
+import stripe
+
+
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
+from profiles.forms import UserProfileForm
+from cart.contexts import cart_contents
+from profiles.models import UserProfile
+from guitars.models import Guitar
+
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 
-from guitars.models import Guitar
-from profiles.models import UserProfile
-from profiles.forms import UserProfileForm
-from cart.contexts import cart_contents
-
-import stripe
-# import json
 
 
-# @require_POST
-# def cache_checkout_data(request):
-#     try:
-#         pid = request.POST.get('client_secret').split('_secret')[0]
-#         stripe.api_key = settings.STRIPE_SECRET_KEY
-#         stripe.PaymentIntent.modify(pid, metadata={
-#             'cart': json.dumps(request.session.get('cart', {})),
-#             'save_info': request.POST.get('save_info'),
-#             'username': request.user,
-#         })
-#         return HttpResponse(status=200)
-#     except Exception as e:
-#         messages.error(request, 'Sorry, your payment cannot be \
-#             processed right now. Please try again later.')
-#         return HttpResponse(content=e, status=400)
+
+
+
+
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -68,15 +76,7 @@ def checkout(request):
                             quantity=item_data,
                         )
                         order_line_item.save()
-                    # else:
-                    #     for size, quantity in item_data['items_by_size'].items():
-                    #         order_line_item = OrderLineItem(
-                    #             order=order,
-                    #             product=product,
-                    #             quantity=quantity,
-                    #             product_size=size,
-                    #         )
-                    order_line_item.save()
+
                 except Guitar.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your cart wasn't found in our database. "
@@ -140,45 +140,44 @@ def checkout(request):
     return render(request, template, context)
 
 
-# def checkout_success(request, order_number):
-#     """
-#     Handle successful checkouts
-#     """
-#     save_info = request.session.get('save_info')
-#     order = get_object_or_404(Order, order_number=order_number)
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
 
-#     if request.user.is_authenticated:
-#         profile = UserProfile.objects.get(user=request.user)
-#         # Attach the user's profile to the order
-#         order.user_profile = profile
-#         order.save()
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
 
-#         # Save the user's info
-#         if save_info:
-#             profile_data = {
-#                 'default_phone_number': order.phone_number,
-#                 'default_country': order.country,
-#                 'default_postcode': order.postcode,
-#                 'default_town_or_city': order.town_or_city,
-#                 'default_street_address1': order.street_address1,
-#                 'default_street_address2': order.street_address2,
-#                 'default_county': order.county,
-#             }
-#             user_profile_form = UserProfileForm(profile_data, instance=profile)
-#             if user_profile_form.is_valid():
-#                 user_profile_form.save()
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
-#     messages.success(request, f'Order successfully processed! \
-#         Your order number is {order_number}. A confirmation \
-#         email will be sent to {order.email}.')
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
-#     if 'cart' in request.session:
-#         del request.session['cart']
+    if 'cart' in request.session:
+        del request.session['cart']
 
-#     template = 'checkout/checkout_success.html'
-#     context = {
-#         'order': order,
-#     }
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
 
-#     return render(request, template, context)
-
+    return render(request, template, context)
