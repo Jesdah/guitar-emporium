5f4b352224fbad1199e27ce1cba388aed9a2db8c
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-10-20 08:32:37 +0000

    create wishlist view

--- guitar_emporium/urls.py
+++ guitar_emporium/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('custom/', include('custom_guitar.urls')),
     path('review/', include('reviews.urls')),
+    path('wishlist/', include('wish_list.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- wish_list/views.py
+++ wish_list/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+from guitars.models import Guitar
+from .models import WishList
+
+def add_to_wishlist(request, guitar_id):
+    """
+    Add to wishlist and if the item is 
+    already selected remove from wishlist
+    """
+    item = Guitar.objects.get(id=guitar_id)
+    obj, created = WishList.objects.get_or_create(user=request.user)
+    if item in obj.wishlist_item.all():
+        obj.wishlist_item.remove(item)
+        return messages.success(request, 'Item has been removed from wishlist!')
+    else:
+        obj.wishlist_item.add(item)
+        return messages.success(request, 'Item has been added to wishlist!')
